18b26afa6f3c24c2664a7adad1fd268fc8fdc85a
galuropek <galuropek@gmail.com>
2020-05-27 22:29:18 +0300

    Crews controller: created show and create methods

--- app/controllers/crews_controller.rb
+++ app/controllers/crews_controller.rb
@@ -4,4 +4,27 @@ class CrewsController < ApplicationController
   def index
     @crews = Crew.all
   end
+
+  def new
+
+  end
+
+  def show
+    @crew = Crew.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:crew].inspect
+    params[:crew][:owner] = current_user.id
+    @crew = Crew.new(crew_params)
+
+    @crew.save
+    redirect_to @crew
+  end
+
+  private
+
+  def crew_params
+    params.require(:crew).permit(:name, :description)
+  end
 end
