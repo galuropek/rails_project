2c9339d3bc0c7d3c0d7f08c793e865d58baf5f4f
galuropek <galuropek@gmail.com>
2020-05-27 21:09:59 +0300

    Crews controller: created index method for show all crews

--- app/controllers/crews_controller.rb
+++ app/controllers/crews_controller.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
 class CrewsController < ApplicationController
+
+  def index
+    @crews = Crew.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'crews#index'
+
+  resources :crews
 end
