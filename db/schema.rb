e0771a01df48043565b73d2d5bc0e2fdcbb347ac
galuropek <galuropek@gmail.com>
2020-05-27 19:41:36 +0300

    Add devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_180312) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "user_details", force: :cascade do |t|
-    t.string "name"
-    t.string "fisrt_name"
-    t.integer "age"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
