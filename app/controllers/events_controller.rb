008a64597e10f6730117f1b27240019b47b3b55e
galuropek <galuropek@gmail.com>
2020-06-04 22:16:41 +0300

    Events: add edit event functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,6 +20,23 @@ class EventsController < ApplicationController
     @event = Event.find(params[:crew_id])
   end
 
+  def edit
+    @event = Event.find(params[:crew_id])
+
+    # render 'show' unless current_user_is_owner?(@crew)
+  end
+
+  def update
+    @crew = Crew.find(params[:id])
+    @event = Event.find(params[:crew_id])
+
+    if @event.update(event_params)
+      redirect_to crew_event_path(@event)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def event_params
